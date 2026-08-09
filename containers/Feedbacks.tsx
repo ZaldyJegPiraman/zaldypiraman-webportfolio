439a9166aae5f01d9c0be8e6c44679a6779c0c15
Zaldy Jeg Piraman <zaldyjeg.piraman@gmail.com>
2026-08-10 00:37:06 +0800

    Fix feedback error2

--- containers/Feedbacks.tsx
+++ containers/Feedbacks.tsx
@@ -6,7 +6,33 @@ import Fade from "react-reveal/Fade";
 
 const Feedbacks = () => {
   return (
-  <div></div>
+    feedbacks && (
+      <Fade bottom duration={2000}>
+        <section className="section section-lg">
+          <Container>
+            <div className="d-flex p-4">
+              <div>
+                <div className="icon icon-lg icon-shape bg-gradient-white shadow rounded-circle text-info">
+                  <i className="fa fa-star text-info" />
+                </div>
+              </div>
+              <div className="pl-4">
+                <h4 className="display-3 text-info">Our Clients Feedback</h4>
+              </div>
+            </div>
+            <Row className="g-3">
+             {feedbacks.map((data: any, i) => {
+                return (
+                  <Col key={i} lg={6}>
+                    <FeedbackCard {...data} />
+                  </Col>
+                );
+              })}
+            </Row>
+          </Container>
+        </section>
+      </Fade>
+    )
   );
 };
 
